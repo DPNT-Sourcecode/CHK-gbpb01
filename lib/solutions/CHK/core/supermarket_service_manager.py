28ba5a7f00ac37227b211f2f4d2e56bec26134ec
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 10:56:42 +0000

    Sat Sep 14 10:56:42 UTC 2019

---
+++ lib/solutions/CHK/core/supermarket_service_manager.py
@@ -0,0 +1,14 @@
+import re
+
+class SupermarketService():
+
+    def __init__(self, checkout, service, item_counter):
+        self.checkout = checkout
+        self.service = service
+        self.item_counter = item_counter
+
+    def get_total_items(self):
+        """
+
+        """
+        
