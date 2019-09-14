77dde1c69cd6c0a843488f4b579a81de19bb3492
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:14:42 +0000

    Sat Sep 14 11:14:42 UTC 2019

--- lib/solutions/CHK/resources/supermarket_resource.py
+++ lib/solutions/CHK/resources/supermarket_resource.py
@@ -1,18 +1,30 @@
 
 class SupermarketResource():
 
+    STORE_ITEMS = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    STORE_DISCOUNT = {'A' : {'offer': 3, 'rate': 150 / 130}, 'B': {'offer': 2, 'rate': 45 / 60}}
+
     def __init__(self, store_units):
         self.store_units = store_units
-        self.store_prices = {'A': 50, 'B': 30, 'C' : 20, 'D': 15}
+        self.store_items = {'A': 50, 'B': 30, 'C' : 20, 'D': 15}
 
     
     def get_offer_price(self):
-        
+        total_checkout = 0
         for item in self.store_units:
+            if type(item) if not str or self._string_formatter(item) not in STORE_ITEMS:
+                return -1
+            elif item[0].isdigit():
+                items = self._string_formatter(item)
+                num_items = int(item[:len(items)])
+                total_checkout = total_sum + ((num_item // ))
+        
+
 
 
     def _string_formatter(self, store_units):
         return ''.join([unit for unit in store_units if not unit.isdigit()])
+
 
 
 
