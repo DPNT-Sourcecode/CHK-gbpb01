41c62e1fd40d911f641ec0a9004786c5390a1c28
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:48:17 +0000

    Sat Sep 14 11:48:17 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,5 +8,29 @@ def checkout(skus):
         Args:
             skus (str): number of items
     """
-    supermarket = SupermarketResource(skus)
-    return supermarket.get_offer_price()
+    def get_char_from_str(string):
+        return "".join([i for i in string if not i.isdigit()])
+    skus = skus.split()
+    total_sum = 0
+    store_skus = {"A": 50, "B": 30, "C": 20, "D": 15}
+    skus_discount = {"A": {
+        "min_prod": 3,
+        "rate": 130 / 150},
+        "B": {
+            "min_prod": 2,
+            "rate": 45 / 60}
+    }
+    for sku in skus:
+        if type(sku) is not str or get_char_from_str(sku) not in store_skus.keys():
+            total_sum = -1
+            break
+        elif sku[0].isdigit():
+            sku_str = get_char_from_str(sku)
+            num = int(sku[:len(sku_str)])
+            total_sum = total_sum + \
+                        ((num // skus_discount[sku_str]["min_prod"]) * store_skus[sku_str] * skus_discount[sku_str]["rate"]) + \
+                        ((num % skus_discount[sku_str]["min_prod"]) * store_skus[sku_str])
+        else:
+            total_sum = total_sum + store_skus[sku]
+
+    return total_sum
--- lib/solutions/CHK/resources/supermarket_resource.py
+++ lib/solutions/CHK/resources/supermarket_resource.py
@@ -17,14 +17,15 @@ class SupermarketResource():
         total_checkout = 0
         for item in self.store_units:
             if type(item) is not str or char_formatter(item) not in self.STORE_ITEMS.keys():
-                return - 1
+                total_checkout = -1
+                break
             elif item[0].isdigit():
                 items = char_formatter(item)
                 num_items = int(item[:len(items)])
                 total_checkout = total_checkout + ((num_items // self.STORE_DISCOUNT[items]['offer']) * self.STORE_ITEMS[items] * self.STORE_DISCOUNT[items]['rate']) + \
                                 ((num_items % self.STORE_DISCOUNT[items]['offer']) * self.STORE_ITEMS[items])
             else:
-                total_checkout += self.STORE_ITEMS[item]
+                total_checkout = total_checkout + self.STORE_ITEMS[item]
         return total_checkout
 
     # def _total_price_offer(self, num_item, items):
@@ -36,6 +37,7 @@ class SupermarketResource():
     #     total_price = total_price + ((num_item // STORE_DISCOUNT[items]['offer']) * STORE_ITEMS[items] * STORE_DISCOUNT[items]['rate']) + \
     #                                 ((num % STORE_DISCOUNT[items]['offer']) * STORE_ITEMS[items])
     #     return total_price
+
 
 
 
