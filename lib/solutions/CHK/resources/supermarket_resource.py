6054c247ca17dfe05ae2d5692116ab2683ffc60d
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:20:42 +0000

    Sat Sep 14 11:20:42 UTC 2019

--- lib/solutions/CHK/resources/supermarket_resource.py
+++ lib/solutions/CHK/resources/supermarket_resource.py
@@ -6,8 +6,6 @@ class SupermarketResource():
 
     def __init__(self, store_units):
         self.store_units = store_units
-        self.store_items = {'A': 50, 'B': 30, 'C' : 20, 'D': 15}
-
     
     def get_offer_price(self):
         total_checkout = 0
@@ -26,6 +24,7 @@ class SupermarketResource():
 
     def _string_formatter(self, store_units):
         return ''.join([unit for unit in store_units if not unit.isdigit()])
+
 
 
 
