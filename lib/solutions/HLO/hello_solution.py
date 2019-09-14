0462babe12da6f40d5a7c9eeb565d0403f9d9218
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 10:29:37 +0000

    Sat Sep 14 10:29:37 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,11 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    """
+    Hello Mihail!
+        Args:
+            friend_name (str) : name of your friend
+    Returns:
+        A string containing a message
+    """
+    return 'Hello, {}!'.format(friend_name.strip())
