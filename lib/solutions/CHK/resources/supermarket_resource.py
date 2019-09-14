c2a8a610af6b405c307bda04ab7009c2a76ec0ea
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:42:15 +0000

    Sat Sep 14 11:42:15 UTC 2019

--- lib/solutions/CHK/resources/supermarket_resource.py
+++ lib/solutions/CHK/resources/supermarket_resource.py
@@ -4,7 +4,7 @@ from solutions.CHK.util.formatter_char import char_formatter
 class SupermarketResource():
 
     STORE_ITEMS = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    STORE_DISCOUNT = {'A' : {'offer': 3, 'rate': 150 / 130}, 'B': {'offer': 2, 'rate': 45 / 60}}
+    STORE_DISCOUNT = {'A' : {'offer': 3, 'rate': 150 / 130}, 'B': {'offer': 2, 'rate': 45 / 60} }
 
     def __init__(self, store_units):
         self.store_units = store_units
@@ -14,6 +14,7 @@ class SupermarketResource():
         Special offer price that that is done by a supermarket based on the
         number of items bought
         """
+        self.store_units = self.store_units.split()
         total_checkout = 0
         for item in self.store_units:
             if type(item) is not str or char_formatter(item) not in self.STORE_ITEMS.keys():
@@ -37,4 +38,5 @@ class SupermarketResource():
     #     total_price = total_price + ((num_item // STORE_DISCOUNT[items]['offer']) * STORE_ITEMS[items] * STORE_DISCOUNT[items]['rate']) + \
     #                                 ((num % STORE_DISCOUNT[items]['offer']) * STORE_ITEMS[items])
     #     return total_price
+
 
